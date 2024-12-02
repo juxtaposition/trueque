7cd43bcfdcdb5b3c006fee71f7434fcb6e5257bc
= <exbios@live.co.uk>
2024-12-01 19:48:17 -0600

    using new db

--- trueque/settings.py
+++ trueque/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'trueque.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db_new.sqlite3',
     }
 }
 
